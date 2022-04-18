2db4a5657835a8e6becef81fe098c5338917f057
scel65 <84318506+scel65@users.noreply.github.com>
2022-04-18 15:18:08 +0530

    Create maxAvgSubArray.cpp
    This Code will calculate the max avg of subarray of size k from an array of size n.
    Since k is constant here we are using sliding window technique for best optimization
    Time Complexity :: O(n)
    Space Complexity :: O(1)

---
+++ CPP/Arrays/maxAvgSubArray.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int maxAvgSubArray(int *a, int n, int k)
+{
+    int maxSum =0;
+    int currSum = 0;
+
+
+    for(int i=0;i<k;i++)
+    {
+        currSum += a[i];
+        if(maxSum < currSum)
+        {
+            maxSum=currSum;
+        }
+    }
+    for(int i=k;i<n;i++)
+    {
+        currSum = currSum + a[i];
+        currSum = currSum - a[i-k]; 
+        if(maxSum < currSum)
+        {
+            maxSum=currSum;
+        }
+    }
+    float result = (static_cast<float>(maxSum))/ (static_cast<float>(k));
+    return result;
+}
+
+int main()
+{
+    int *arr = new int[7]{-1, -4, 0, 5, 3, 2, 1} ;
+    cout<<"\n The max average is "<<maxAvgSubArray(arr,7,3)<<endl;
+
+    delete [] arr;
+    return 0;
+
+}
